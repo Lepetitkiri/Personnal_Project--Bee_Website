bab9ccb1cbdf48a5a463e10105e2245331607ce1
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2024-02-24 12:26:22 +0100

    Conditional operator added in Card component to create ul tags

--- src/Components/Card/Index.jsx
+++ src/Components/Card/Index.jsx
@@ -12,7 +12,19 @@ function Card() {
         <article key={`HomeDatas-${index}`}>
           <h2> {HomeDatas[index].titre} </h2>
           <h3> {HomeDatas[index]["sous-titre"]} </h3>
-          <p> {HomeDatas[index].texte} </p>
+
+          {/* Code de vérification de la présence de texte2/3/4 : */}
+          {HomeDatas[index].texte2 ? (
+            <ul>
+              <li> {HomeDatas[index].texte1} </li>
+              <li> {HomeDatas[index].texte2} </li>
+              {HomeDatas[index].texte3 && <li> {HomeDatas[index].texte3} </li>}
+              {HomeDatas[index].texte4 && <li> {HomeDatas[index].texte4} </li>}
+            </ul>
+          ) : (
+            <p> {HomeDatas[index].texte1} </p>
+          )
+          }
         </article>
 
       ))}
