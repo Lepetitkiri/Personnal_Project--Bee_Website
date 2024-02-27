75687e86b6bda9ac75261973ed6f4197b995cee2
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2024-02-27 10:26:58 +0100

    Prop utilisation in Header bug fixed

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from 'react-router-dom';
-// Validation de la prop pour utilisation dans le styled component
-import { StyleSheetManager } from 'styled-components';
-import isPropValid from '@emotion/is-prop-valid';
 import HeaderStyle from "./Style.jsx";
 
 function Header({ basename }) {
   return (
-    <StyleSheetManager shouldForwardProp={prop => isPropValid(basename)}>
-      <HeaderStyle basename={basename} >
-        <img alt="Abeille" src={`${basename}/Pictures/Bee_Logo.png`} />
-        <h1>Votre apiculteur local à Rivière dans le Pas-de-calais</h1>
-        <nav>
-          <Link to="/"> Accueil </Link>
-          <Link to="/Boutique"> Boutique </Link>
-        </nav>
-      </HeaderStyle>
-    </StyleSheetManager>
+    <HeaderStyle basename={basename} >
+      <img alt="Abeille" src={`${basename}/Pictures/Bee_Logo.png`} />
+      <h1>Votre apiculteur local à Rivière dans le Pas-de-calais</h1>
+      <nav>
+        <Link to="/"> Accueil </Link>
+        <Link to="/Boutique"> Boutique </Link>
+      </nav>
+    </HeaderStyle>
   );
 }
 
