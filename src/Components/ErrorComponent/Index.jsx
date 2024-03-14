bf3f245ab590ce23516a147523615220c83e41f9
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2024-03-14 15:30:24 +0100

    ErrorComponent started

--- src/Components/ErrorComponent/Index.jsx
+++ src/Components/ErrorComponent/Index.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import PropTypes from "prop-types";
 import ErrorComponentStyle from "./Style.jsx";
 
-// Componenets
-import WorkInProgress from "../WorkInProgress/Index.jsx";
+import ShopPartStyle from '../ShopPart/Style.jsx';
 
 function ErrorComponent({ basename }) {
+  const [isChecked, setIsChecked] = useState(Array(9).fill(false)); // Tableau d'états pour les boutons
+
+  const handleButtonClick = (index) => {
+    const updatedChecked = [...isChecked];
+    updatedChecked[index] = !updatedChecked[index];
+    setIsChecked(updatedChecked);
+  };
 
   return (
     <ErrorComponentStyle basename={basename}>
       <p>Oups un erreur</p>
       <Link className="Error--Link" to="/"> Cliquez ici pour retourner à l'accueil </Link>
-      <WorkInProgress basename={basename} />
+      <ShopPartStyle className="Error--Instructions">
+        <p> Maintenant c'est à vous de travailler. Cliquez sur toutes les fleurs pour les butiner🌱 </p>
+      </ShopPartStyle>
+      <div className="Error--Game">
+        {isChecked.map((checked, index) => (
+          <button key={index} onClick={() => handleButtonClick(index)}>
+            {checked ? <img alt="Fleur blanche" src={`${basename}/Pictures/flower-empty.png`} /> : <img alt="Fleur rose" src={`${basename}/Pictures/flower-full.png`} />}
+          </button>
+        ))}
+      </div>
     </ErrorComponentStyle>
   );
 }
--- src/Components/ErrorComponent/Style.jsx
+++ src/Components/ErrorComponent/Style.jsx
@@ -6,6 +6,7 @@ const ErrorComponentStyle = styled.div`
 
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: ${colors.tertiary};
   margin-bottom: 10px;
   width: 100%;
@@ -18,29 +19,47 @@ const ErrorComponentStyle = styled.div`
       color: ${colors.black};
       text-align: center;
     }
+    .Error--Instructions {
+      margin: 50px 0 50px 0;
+    }
     img {
-      border-radius: 20px;
-      width: 30%;
+      width: 100%;
       object-fit: contain;
     }
-  }
+    button {
+      width: 8%;
+      border: none;
+      background: none;
+      padding: 0;
+      margin: 0;
+      font: inherit;
+      color: inherit;
+      cursor: url('${props => props.basename}/Pictures/bee.png'), auto;
+    }
+    .Error--Game {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 60%;
+      height: auto;
+      & button:nth-child(1), button:nth-child(5), button:nth-child(9) {
+        align-self: start;
+      }
+      & button:nth-child(3), button:nth-child(7) {
+        align-self: end;
+      }
+    }
 
 
   /* Version tablette */
   @media only screen and (max-width: 992px) {
-      img {
-        width: 40%;
+      button {
+        width: 12%;
       }
   }
 
   /* Version mobile */
   @media only screen and (max-width: 767px) {
-    img {
-      width: 100%;
-      height: auto;
-      max-height: 200px;
-      object-fit: contain;
-    }
   }
 `;
 
